4ef6b6e76ec9edb38c69056b0f861e9095b5fc2d
duracell27 <volodymyrshmidt@gmail.com>
2022-06-30 17:52:23 +0300

    almost filter done

--- src/components/tournaments/tournamentsTest.js
+++ src/components/tournaments/tournamentsTest.js
@@ -98,22 +98,6 @@ const CustomCheckbox = (props) => {
   }
 };
 
-const makePreFilteredArray = (filteredObj, data, filterType) => {
-  for (let key in filteredObj) {
-    let filteredTournaments
-    if (filteredObj[key] === true) {
-      filteredTournaments = data.filter((dataItem) => {
-        return dataItem[filterType] === key
-      })
-    }
-    if (filteredTournaments) {
-      return filteredTournaments
-    }else{
-      return null
-    }
-  }
-}
-
 const TournamentTable2 = ({ dataFilter, formatFilter, appFilter }) => {
   const columns = React.useMemo(() => [
     {
@@ -165,7 +149,7 @@ const TournamentTable2 = ({ dataFilter, formatFilter, appFilter }) => {
 
   useEffect(() => {
     Papa.parse(
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vTfWriFU7Z6ldxCNhoIhLJvZrbkUsarIl-uSmEcvtwW8xwtjc64LpmFjfHw4zfqrGhev80cgkSFUt3t/pub?output=csv',
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vR7yTwQvKlRgYsdrgv9mnbrhJoRM3_1tck9irwimNoBpRPgU46NIAdxcPu645-8Y8iicgH9ZhuK40YD/pub?gid=0&single=true&output=csv',
       {
         download: true,
         header: true,
@@ -178,9 +162,9 @@ const TournamentTable2 = ({ dataFilter, formatFilter, appFilter }) => {
   }, []);
 
 
-  console.log('DATAFILTER', dataFilter);
-  console.log('FORMATFILTER', formatFilter);
-  console.log('APPFILTER', appFilter);
+  // console.log('DATAFILTER', dataFilter);
+  // console.log('FORMATFILTER', formatFilter);
+  // console.log('APPFILTER', appFilter);
 
 
   const options = {
@@ -194,6 +178,7 @@ const TournamentTable2 = ({ dataFilter, formatFilter, appFilter }) => {
     selectableRowsHeader: false,
     selectableRowsHideCheckboxes: true, // TUT
     selectableRowsOnClick: true,
+    rowsPerPage: 50,
     filterType: {
       customFilterListOptions: () => {
         return <div>hui</div>;
@@ -201,37 +186,86 @@ const TournamentTable2 = ({ dataFilter, formatFilter, appFilter }) => {
     }
   };
   const tournaments = Array.from(data);
-  let filteredTournaments = [];
+
+  // time filter
+  let filteredTournamentsAll = [];
+  // console.log('data', new Date())
+  // const year = tournaments[0].data.slice(6, 10)
+  // const month = tournaments[0].data.slice(3, 5)
+  // const day = tournaments[0].data.slice(0, 2)
+  // console.log('data1', new Date(year, month - 1, day))
+  // const minus = new Date() - new Date(year, month - 1, day)
+  // console.log('minus', minus / 1000)
+  for (let key in dataFilter) {
+    let filteredTournaments0
+    if (dataFilter[key] === true) {
+      filteredTournaments0 = data.filter((dataItem) => {
+        const year = dataItem.data.slice(6, 10)
+        const month = dataItem.data.slice(3, 5)
+        const day = dataItem.data.slice(0, 2)
+        const tournamentData = new Date(year, month - 1, day) 
+        const timeToTournament = ((tournamentData - new Date()) / 1000) + 86399
+
+        const now = new Date();
+        const tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
+
+        const timeToTomorrow = (tomorrow - now) / 1000;
+
+        // console.log('timeToTomorrow', timeToTomorrow)
+        // console.log('timeToTournament', timeToTournament)
+        // console.log('item', dataItem.name)
+
+        if (key === 'today' && timeToTournament > 0 && timeToTournament < timeToTomorrow) {
+          return 1
+        }
+        if (key === 'tomorrow' && timeToTournament > 86400 && timeToTournament < 172800) {
+          return 1
+        }
+        if (key === 'week' && timeToTournament > 0 && timeToTournament < 604800) {
+          return 1
+        }
+
+      })
+    }
+    if (filteredTournaments0) {
+      filteredTournamentsAll.push(...filteredTournaments0)
+    }
+  }
+
   // format filter
-  filteredTournaments.push(...makePreFilteredArray(formatFilter, tournaments, 'type'))
-  // for (let key in formatFilter) {
-  //   let filteredTournaments1
-  //   if (formatFilter[key] === true) {
-  //     filteredTournaments1 = data.filter((dataItem) => {
-  //       return dataItem.type === key
-  //     })
-  //   }
-  //   if (filteredTournaments1) {
-  //     return
-  //   }
-  // }
-  // end format filter
-  // filteredTournaments.push(...filteredTournaments1)
+
+  for (let key in formatFilter) {
+    let filteredTournaments1
+    if (formatFilter[key] === true) {
+      filteredTournaments1 = data.filter((dataItem) => {
+        return dataItem.type === key
+      })
+    }
+    if (filteredTournaments1) {
+      filteredTournamentsAll.push(...filteredTournaments1)
+    }
+  }
+
   // app filter
 
-  // let filteredTournaments2 = []
-  // for (let key in appFilter) {
-  //   if (appFilter[key] === true) {
-  //     filteredTournaments2 = filteredTournaments.filter((dataItem) => {
-  //       return dataItem.application === key
-  //     })
-  //   }
-  // }
-  // app format filter
+  for (let key in appFilter) {
+    let filteredTournaments2
+    if (appFilter[key] === true) {
+      filteredTournaments2 = data.filter((dataItem) => {
+        return dataItem.application === key
+      })
+    }
+    if (filteredTournaments2) {
+      filteredTournamentsAll.push(...filteredTournaments2)
+    }
+  }
+
+
+  console.log('filteredTournamentsAll', filteredTournamentsAll)
   return (
     <ThemeProvider theme={theme}>
       <MUIDataTable
-        data={filteredTournaments.length ? filteredTournaments : tournaments}
+        data={filteredTournamentsAll.length ? filteredTournamentsAll : tournaments}
         columns={columns}
         options={options}
       // components={{
